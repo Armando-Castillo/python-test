0b67cf25f9554dd25ec12c289f13cc074b7c6c7e
Armando-Castillo <armandocreyes02@gmail.com>
2021-10-13 17:15:36 -0500

    clean update and aggregate func

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from datetime import date, datetime
-from numpy import int0, int16, string_
+from datetime import date
+from numpy import int16
 import pandas as pd
 import os 
 
@@ -8,11 +8,12 @@ import os
 def main():
   #Read path for data source
   data_source = input("Ingrese ruta/path del data source: ")
-  df = read_data(data_source)
+  df = read_data('/home/armando/colektia/clientes.csv')
   clean_df = clean_data(df)
-  print(clean_df['due_date'])
-  # df_transformed = transform_data(df)
-  # load_data(df_transformed)
+  if etl_process(clean_df):
+    print("ETL process complete")
+  else:
+    print("Algo salió mal")
  
   
 def read_data(data_source):
@@ -35,40 +36,57 @@ def clean_data(df):
     'telefono': 'phone'
   })
   #DATA MISSING HANDLING
-  df['email'] = df['email'].fillna(value='None')
-  df['status'] = df['status'].fillna(value='None')
-  df['priority'] = df['priority'].fillna(value=0)
-  df['phone'] = df['priority'].fillna(value=0)
+  df[['email', 'status']] = df[['email', 'status']].fillna(value='None')
+  df[['priority', 'phone']] = df[['priority', 'phone']].fillna(value=0)
   #DATA STRUCTURE
-  df['fiscal_id'] = df['fiscal_id'].astype('string')
-  df['first_name'] = df['first_name'].astype('string')
-  df['last_name'] = df['last_name'].astype('string')
-  df['gender'] = df['gender'].astype('string')
+  df[['fiscal_id', 'first_name', 'last_name', 'gender', 'address', 'email', 'status', 'phone']] = df[[
+    'fiscal_id', 'first_name', 'last_name', 'gender', 'address', 'email', 'status', 'phone'
+  ]].astype('string')
   df['birth_date'] = pd.to_datetime(df['birth_date'])
   df['due_date'] = pd.to_datetime(df['due_date'])
-  df['address'] = df['address'].astype('string')
-  df['email'] = df['email'].astype('string')
-  df['status'] = df['status'].astype('string')
   df['priority'] = df['priority'].astype(int16)
-  df['phone'] = df['phone'].astype('string')
   return df
 
 
-def transform_data(df):
-  #df_clean = clean_df(df)
-  clients_df = create_clientes_df(df_clean)
-  return clients_df
+def etl_process(df):
+  clients_df = transform_clientes_df(df)
+  print(clients_df)
+  return 1
   
 
 def load_data(df):
   dir_path = os.path.dirname(os.path.realpath(__file__)) + '/output/'
   location_file = dir_path + 'clientes.xlsx'
   df.to_excel(location_file)
+
  
-def create_clientes_df(df):
-  clientes_df = df[["fiscal_id", "first_name", "last_name", "gender"]]
-  clientes_df["birth_date"] = df["fecha_nacimiento"] 
-  return clientes_df
+def transform_clientes_df(df):
+  df['age'] = df['birth_date'].apply(lambda x: get_age(x))
+  df['age_group'] = df['age'].apply(lambda x: get_age_group(x))
+  clients_df = df[['age', 'age_group']]
+  return clients_df
+
+
+def get_age(born):
+    today = date.today()
+    age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+    return age
+  
+
+def get_age_group(age):
+    if age <= 20:
+      age_group = 1
+    elif age > 20 & age <= 30:
+      age_group = 2
+    elif age > 30 & age <= 40:
+      age_group = 3
+    elif age > 40 & age <= 50:
+      age_group = 4
+    elif age > 50 & age <= 60:
+      age_group = 5
+    else:
+      age_group = 6
+    return age_group
 
 if __name__ == '__main__':
   main()
