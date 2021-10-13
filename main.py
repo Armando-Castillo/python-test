1d0ad81fb8849757d2525e3678ddba30bd16d650
Armando-Castillo <armandocreyes02@gmail.com>
2021-10-13 07:09:30 -0500

    get path

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pandas as pd
+import os 
 
 #/home/armando/colektia/clientes.csv
     
 def main():
+  #Read path for data source
   data_source = input("Ingrese ruta/path del data source: ")
+  df_transformed = extract_data(data_source)
+  print(df_transformed)
+  load_data(df_transformed)
+ 
+  
+def extract_data(data_source):
   print(data_source)
-
   df = pd.read_csv(data_source, sep=';')
-  print(df)
+  df_transformed = transform_data(df)
+  return df_transformed
+  
 
-  data_target = input("Ingrese ruta/path del data target: ")
-  print(data_target)
+def transform_data(df):
+  return pd.DataFrame(df)
+  
+
+def load_data(df):
+  dir_path = os.path.dirname(os.path.realpath(__file__)) + '/output/'
+  location_file = dir_path + 'clientes.xlsx'
+  df.to_excel(location_file)
+  
   
 if __name__ == '__main__':
   main()
